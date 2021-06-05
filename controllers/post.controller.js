06314e54dfd8266bda162d52443927af892d38df
Nguyễn Phú <71036328+Phu0903@users.noreply.github.com>
2021-06-06 03:35:11 +0700

    commit

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -159,6 +159,7 @@ module.exports = {
 
                 }
             } catch (error) {
+                console.log(error);
                 res.status(500).json({
                     success: false,
                     message: error.message
