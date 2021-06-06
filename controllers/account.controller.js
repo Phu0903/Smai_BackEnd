7f03350aebdd2aef215b18e85be17a1ac0594b6a
Nguyễn Phú <71036328+Phu0903@users.noreply.github.com>
2021-06-06 13:11:06 +0700

    merge

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -4,220 +4,115 @@ const User = require('../Model/User')
 const jwt = require('jsonwebtoken');
 require("dotenv").config();
 
-<<<<<<< HEAD
+
 module.exports ={
-     
-    //Login
-    login : async(req,res) => {
-        const {UserName,Password} = req.body
-        if( !UserName || !Password )
-            return res 
-                  .status(400)
-                  .json({
-                    success:false,
-                    message:"Missing user name or password"
-                  })
-        try{
-          const user = await Account.findOne({'UserName':UserName})
-          const passwordValid = await argon2d.verify(user.Password,Password)
-          if(!user || !passwordValid) 
-            return res 
-                   .status(400)
-                   .json({
-                     success:false,
-                     'message':'Incorrect username or password'
-                   })
-          const accessToken = jwt.sign(
-                     { accountID: user._id },
-                      process.env.ACCESS_TOKEN_SECRET
-                      )
-           res.json({
-              success:true,
-              "message": "OK",
-              "accessToken":accessToken,
+ //Login
+ login : async(req,res) => {
+  const {UserName,Password} = req.body
+  if( !UserName || !Password )
+      return res 
+            .status(400)
+            .json({
+              success:false,
+              message:"Missing user name or password"
             })
-            
-        }catch(err)
-        {
-          res.status(500).json({
-=======
-module.exports = {
-
-  //Login
-  login: async (req, res) => {
-    const { UserName, Password } = req.body
-    if (!UserName || !Password)
-      return res
-        .status(400)
-        .json({
-          success: false,
-          message: "Missing user name or password"
-        })
-    try {
-      const user = await Account.findOne({ 'UserName': UserName })
-      const passwordValid = await argon2d.verify(user.Password, Password)
-      if (!user || !passwordValid)
-        return res
-          .status(400)
-          .json({
->>>>>>> 03822a78ebb5edba76670d9d39d67fa956fc4b01
-            success: false,
-            message: 'Incorrect username or password'
-          })
-      const accessToken = jwt.sign(
-        { accountID: user._id },
-        process.env.ACCESS_TOKEN_SECRET
-      )
-      res.json({
-        success: true,
-        message: 'User logged in successfully',
-        accessToken
+  try{
+    const user = await Account.findOne({'UserName':UserName})
+    const passwordValid = await argon2d.verify(user.Password,Password)
+    if(!user || !passwordValid) 
+      return res 
+             .status(400)
+             .json({
+               success:false,
+               'message':'Incorrect username or password'
+             })
+    const accessToken = jwt.sign(
+               { accountID: user._id },
+                process.env.ACCESS_TOKEN_SECRET
+                )
+     res.json({
+        success:true,
+        "message": "OK",
+        "accessToken":accessToken,
       })
+      
+  }catch(err)
+  {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+},
 
-    } catch (err) {
-      res.status(500).json({
-        success: false,
-        message: err.message
-      });
-    }
-  },
-
-<<<<<<< HEAD
-      //register
-    register: async (req, res) => {
-        const {UserName,Password,PhoneNumber,Address,FullName} = req.body
-        console.log(req.body);
-        if (!UserName || !Password) {
-          console.log("1");
-          return res
-            .status(400)
-            .json({
-              success: false,
-              "message": "UserName or Password not exist"
-            })
-        }
-        if(!PhoneNumber)
-        {
-          console.log("2");
-          return res 
-                .status(400)
-                .json({
-                  success:false,
-                  "message":"PhoneNumber not exits"
-                })
-        }
-            try {
-          const user = await Account.findOne({'UserName':UserName })
-          if (user){
-            console.log("UserName already taken");
-            return res
-            .status(400)
-            .json({
-                success: false,
-                "message": 'UserName already taken'
-              })
-            }
-          else {
-            const hashedPassword = await argon2d.hash(Password)//hasd pass word by argon 
-            const data = new Account({
-              'UserName': UserName,
-              'Password': hashedPassword,
-              'PhoneNumber' : PhoneNumber,
-              
-            })
-            const UserDetail = new User({
-              'UserName':UserName,
-              'PhoneNumber':PhoneNumber,
-              'AccountID': data._id,
-              'FullName':FullName,
-              'Address':Address
-            })
-            const accessToken = jwt.sign(
-              { accountID: data._id },
-               process.env.ACCESS_TOKEN_SECRET)
-             
-            data.save(function (err) {
-              UserDetail.save(),
-              res.status(201)
-                .json({
-                  success: true,
-                  "message": "OK",
-                  "accessToken":accessToken, //Respone token for client user
-                 
-                })
-            }); 
-          }
-        } catch (err) {
-          res.status(500).json({
-            success: false,
-           "message": err.message
-          });
-        }
-=======
-
-  //register
-  register: async (req, res) => {
-    const { UserName, Password, PhoneNumber } = req.body
+   //register
+   register: async (req, res) => {
+    const {UserName,Password,PhoneNumber,Address,FullName} = req.body
+    console.log(req.body);
     if (!UserName || !Password) {
+      console.log("1");
       return res
         .status(400)
         .json({
           success: false,
-          message: "UserName or Password not exist"
+          "message": "UserName or Password not exist"
         })
     }
-    if (!PhoneNumber) {
-      return res
-        .status(400)
-        .json({
-          success: false,
-          message: "PhoneNumber not exits"
-        })
+    if(!PhoneNumber)
+    {
+      console.log("2");
+      return res 
+            .status(400)
+            .json({
+              success:false,
+              "message":"PhoneNumber not exits"
+            })
     }
-    try {
-      const user = await Account.findOne({ 'UserName': UserName })
-      if (user)
+        try {
+      const user = await Account.findOne({'UserName':UserName })
+      if (user){
+        console.log("UserName already taken");
         return res
-          .status(400)
-          .json({
+        .status(400)
+        .json({
             success: false,
-            message: 'UserName already taken'
+            "message": 'UserName already taken'
           })
-
+        }
       else {
         const hashedPassword = await argon2d.hash(Password)//hasd pass word by argon 
         const data = new Account({
           'UserName': UserName,
           'Password': hashedPassword,
-          'Rule': 1,
-          'PhoneNumber': PhoneNumber,
-
+          'PhoneNumber' : PhoneNumber,
+          
         })
         const UserDetail = new User({
-          'UserName': UserName,
-          'PhoneNumber': PhoneNumber,
-          'AccountID': data._id
+          'UserName':UserName,
+          'PhoneNumber':PhoneNumber,
+          'AccountID': data._id,
+          'FullName':FullName,
+          'Address':Address
         })
         const accessToken = jwt.sign(
           { accountID: data._id },
-          process.env.ACCESS_TOKEN_SECRET)
-
+           process.env.ACCESS_TOKEN_SECRET)
+         
         data.save(function (err) {
           UserDetail.save(),
-            res.status(201)
-              .json({
-                success: true,
-                message: "OK",
-                "accessToken": accessToken, //Respone token for client user
-
-              })
-        });
->>>>>>> 03822a78ebb5edba76670d9d39d67fa956fc4b01
+          res.status(201)
+            .json({
+              success: true,
+              "message": "OK",
+              "accessToken":accessToken, //Respone token for client user
+             
+            })
+        }); 
       }
     } catch (err) {
       res.status(500).json({
         success: false,
-        message: err.message
+       "message": err.message
       });
     }
   }
