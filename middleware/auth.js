f900fd53dfcdd102a7c94e9e68840945f685f0b9
Nguyễn Phú <71036328+Phu0903@users.noreply.github.com>
2021-06-08 18:35:27 +0700

    commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -19,7 +19,10 @@ const verifyToken = (req,res,next) =>{
         next() // cho qua
     } catch (error) {
         console.log(error)
-        return res.status(403).json(error)
+        return res.status(403).json({
+            success:false,
+            'message':"Invalid token"
+        })
     }
 }
 module.exports = verifyToken
