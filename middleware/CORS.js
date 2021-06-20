43d53f54c9218fda12db80fd90f85fd46ba13a49
Nguyễn Phú <71036328+Phu0903@users.noreply.github.com>
2021-06-20 23:14:28 +0700

    make it better

--- middleware/CORS.js
+++ middleware/CORS.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 const CORS = (req, res, next) => {
     req.header("Access-Control-Allow-Origin", "*");
-    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");.
+    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
