abaa8d702a63b6a54a61c43524fd82ad2f90f492
Tuan Anh <18520446@gm.uit.edu.vn>
2021-06-22 23:43:13 +0700

    tuan anh add logout function

--- routes/frontendRouter/home.router.js
+++ routes/frontendRouter/home.router.js
@@ -2,10 +2,11 @@ const express = require('express');
 const client = express.Router();
 const controller = require('../../controllers/frontendController/home');
 const CORS = require('../../middleware/CORS')
+const verifyToken = require('../../middleware/auth_web')
 
-client.get('/', CORS, controller.getProduct);
+client.get('/', verifyToken, CORS, controller.getProduct);
 
-client.get('/view-post',CORS,controller.getDetail)
+client.get('/view-post', verifyToken, CORS, controller.getDetail)
 
 
 module.exports = client;
