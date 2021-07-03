7caf8b4f4e73a1d99a09f1fd61014a90866c2d77
Nguyễn Phú <71036328+Phu0903@users.noreply.github.com>
2021-07-04 01:51:24 +0700

    make it better

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -107,6 +107,7 @@ module.exports = {
 
             }
         } catch (error) {
+            console.log(error)
             res.status(500).json({
                 success: false,
                 message: error.message
