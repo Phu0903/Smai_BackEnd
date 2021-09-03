2bc8c138b8f324d6decf9949166181210599f4c6
Nguyễn Phú <71036328+Phu0903@users.noreply.github.com>
2021-09-04 01:47:44 +0700

    get, create

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -1,4 +1,5 @@
-const Post = require("../Model/Transaction");
+const Transaction = require("../Model/Transaction");
+const User = require("../Model/User");
 
 const MessageResponse = (success, message, data) => {
   return {
@@ -14,40 +15,125 @@ module.exports = {
   createTransaction: async (req, res) => {
     //req body
     const {
-      //Id người sở hữu bài viết sinh ta giao dịch
-      ReceiverID,
-      PostID, //ID bài viết sinh ra giao dịch
-      SenderAddress, //Địa chỉ
-      Title, //title
-      Note, //note
-      IsConnect, //isConnect
-      IsConfirm, //isConfirm
+      receiverID, //Id người sở hữu bài viết sinh ta giao dịch
+      postID, //ID bài viết sinh ra giao dịch
+      senderAddress, //Địa chỉ
+      title, //title
+      note, //note
+      isConnect, //isConnect
+      isConfirm, //isConfirm
     } = req.body;
     try {
       if (
-        !ReceiverID ||
-        !PostID ||
-        !SenderAddress ||
-        !Title ||
-        !IsConnect ||
-        !IsConfirm
+        !receiverID ||
+        !postID ||
+        !senderAddress ||
+        !title ||
+        !isConnect ||
+        !isConfirm
       ) {
-         res.status(400).json(
-             MessageResponse(
-                 false, 
-                 "The parameters are not enough",
-                 )
-           );
-      }else{
-          
+        res
+          .status(400)
+          .json(MessageResponse(false, "The parameters are not enough"));
+      }
+      if (!req.accountID) {
+        res.status(400).json(MessageResponse(false, "No have accound ID"));
+      } else {
+        //id người tạo ra giao dịch
+        const senderID = await User.findOne({ AccountID: req.accountID });
+        if (!senderID) {
+          return res
+            .status(400)
+            .json(MessageResponse(false, "No have SenderID"));
+        } else {
+          //check img
+          let pathImage = [];
+          if (!req.files) {
+            pathImage = [];
+          } else {
+            req.files.map(function (files) {
+              pathImage.push(files.path);
+            });
+          }
+          //check true false
+          let checkConnect, checkConfirm;
+          if (isConnect == "True" || isConnect == "true") {
+            checkConnect = true;
+          }
+          if (isConnect == "False" || isConnect == "false") {
+            checkConnect = false;
+          }
+          if (isConfirm == "True" || isConfirm == "true") {
+            checkConfirm = true;
+          }
+          if (isConfirm == "False" || isConfirm == "false") {
+            checkConfirm = false;
+          }
+          //lưu vào db Transaction
+          const dataTransaction = await new Transaction({
+            SenderID: senderID,
+            ReceiverID: receiverID,
+            PostID: postID,
+            SenderAddress: senderAddress,
+            title: title,
+            note: note,
+            isConnect: checkConnect,
+            isConfirm: checkConfirm,
+            urlImage: pathImage,
+          });
+          dataTransaction.save(function (err) {
+            if (err) {
+              res.status(400).json(MessageResponse(false, "save db error"));
+            } else {
+              res
+                .status(201)
+                .json(MessageResponse(true, "create transaction success"));
+            }
+          });
+        }
       }
     } catch (error) {
-        res.status(500).json(
-            MessageResponse(
-                false,
-                error.message
-            )
-        )
+      res.status(500).json(MessageResponse(false, error.message));
+    }
+  },
+  //get Transaction mà mình quan tâm
+  getTransactionSenderID: async (req, res) => {
+    try {
+      const accountId = await User.findOne({ AccountID: req.accountID });
+      if (!accountId) {
+        return res.status(400).json(MessageResponse(false, "No have SenderID"));
+      } else {
+        const transaction = await Transaction.find({ SenderID: accountId });
+        if (!transaction) {
+          res.status(404).json(MessageResponse(fasle, "Not Found"));
+        } else {
+          res
+            .status(200)
+            .json(MessageResponse(true, "Find Success", transaction));
+        }
+      }
+    } catch (error) {
+      res.status(500).json(MessageResponse(false, error.message));
+    }
+  },
+  //get Transaction mà mình được quan tâm
+  getTransactionReceiverID: async (req, res) => {
+    try {
+      const accountId = await User.findOne({ AccountID: req.accountID });
+      if (!accountId) {
+        return res.status(400).json(MessageResponse(false, "No have SenderID"));
+      } else {
+        const transaction = await Transaction.find({ ReceiverID: accountId });
+        if (!transaction) {
+          res.status(404).json(MessageResponse(fasle, "Not Found"));
+        } else {
+          res
+            .status(200)
+            .json(MessageResponse(true, "Find Success", transaction));
+        }
+      }
+    } catch (error) {
+      res.status(500).json(MessageResponse(false, error.message));
     }
   },
 };
--- routes/transaction.router.js
+++ routes/transaction.router.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const transactionController = require("../controllers/transaction.controller");
 const transaction = express.Router();
+const verifyToken = require("../middleware/auth");
+const fileUploader = require("../configs/cloudinary.config");
 
-transaction.post("/create-transaction", transactionController.createTransaction);
-
+transaction.post("/create-transaction",verifyToken,fileUploader.array('productImage'), transactionController.createTransaction);
+transaction.get("/transaction-sender", verifyToken, transactionController.getTransactionSenderID);
+transaction.get("/transaction-receiver",verifyToken,transactionController.getTransactionReceiverID);
 module.exports = transaction;
